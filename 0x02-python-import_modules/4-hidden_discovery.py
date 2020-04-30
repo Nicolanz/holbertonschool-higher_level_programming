0a860e77ddab4143eeabf5b9d793c66d877073ea
Nicolanz <zarateguarinnicolasandres@gmail.com>
2020-04-30 22:55:48 +0000

    Task #4 updated

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3.4
-import hidden_4
-names = dir(hidden_4)
-b = 0
-for i in names:
-    if (i[b] != '_'):
-        print(i)
+if __name__ == "__main__":
+    import hidden_4
+    names = dir(hidden_4)
+    b = 0
+    for i in names:
+        if (i[b] != '_'):
+            print(i)

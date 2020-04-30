efbc5491859d457aadb5ee054a531e86500d5ba3
Nicolanz <zarateguarinnicolasandres@gmail.com>
2020-04-30 21:01:17 +0000

    Task #2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+from sys import argv
+if __name__ == "__main__":
+    size = len(argv) - 1
+    if (size < 1):
+        print("0 arguments.")
+    elif (size == 1):
+        print("{:d} argument:".format(size))
+    else:
+        print("{:d} arguments:".format(size))
+    for i in range(1, len(argv)):
+        print("{:d}: {:s} ".format(i, argv[i]))

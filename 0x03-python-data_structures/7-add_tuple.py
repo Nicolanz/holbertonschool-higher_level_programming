3cabad396799e04730821c233901203cc87fb1f5
Nicolanz <zarateguarinnicolasandres@gmail.com>
2020-05-06 02:35:50 +0000

    Task #7

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    if (len(tuple_a) >= 2 and len(tuple_b) >= 2):
+        a = tuple_a[0] + tuple_b[0]
+        b = tuple_a[1] + tuple_b[1]
+    elif (len(tuple_a) < 1 and len(tuple_b) < 1):
+        a = 0
+        b = 0
+    elif (len(tuple_a) < 2 and len(tuple_b) < 1):
+        a = tuple_a[0] + 0
+        b = 0
+    elif (len(tuple_a) < 1 and len(tuple_b) < 2):
+        a = 0 + tuple_b[0]
+        b = 0
+    elif (len(tuple_a) == 2 and len(tuple_b) < 1):
+        a = tuple_a[0] + 0
+        b = tuple_a[1] + 0
+    elif (len(tuple_a) < 1 and len(tuple_b) == 2):
+        a = 0 + tuple_b[0]
+        b = 0 + tuple_b[1]
+    elif (len(tuple_a) >= 2 and len(tuple_b) < 2):
+        a = tuple_a[0] + tuple_b[0]
+        b = tuple_a[1] + 0
+    elif (len(tuple_a) < 2 and len(tuple_b) >= 2):
+        a = tuple_a[0] + tuple_b[0]
+        b = 0 + tuple_b[1]
+    tup = (a, b)
+    return(tup)

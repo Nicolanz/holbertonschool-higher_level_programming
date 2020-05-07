75d96510091bb4d37e54fefb7ee1d7b837198c45
Nicolanz <zarateguarinnicolasandres@gmail.com>
2020-05-07 01:15:22 +0000

    Task #7 re-corrected

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,28 +1,24 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    if (len(tuple_a) >= 2 and len(tuple_b) >= 2):
-        a = tuple_a[0] + tuple_b[0]
-        b = tuple_a[1] + tuple_b[1]
-    elif (len(tuple_a) < 1 and len(tuple_b) < 1):
-        a = 0
-        b = 0
-    elif (len(tuple_a) < 2 and len(tuple_b) < 1):
-        a = tuple_a[0] + 0
-        b = 0
-    elif (len(tuple_a) < 1 and len(tuple_b) < 2):
-        a = 0 + tuple_b[0]
-        b = 0
-    elif (len(tuple_a) >= 2 and len(tuple_b) < 1):
-        a = tuple_a[0] + 0
-        b = tuple_a[1] + 0
-    elif (len(tuple_a) < 1 and len(tuple_b) >= 2):
-        a = 0 + tuple_b[0]
-        b = 0 + tuple_b[1]
-    elif (len(tuple_a) >= 2 and len(tuple_b) < 2):
-        a = tuple_a[0] + tuple_b[0]
-        b = tuple_a[1] + 0
-    elif (len(tuple_a) < 2 and len(tuple_b) >= 2):
-        a = tuple_a[0] + tuple_b[0]
-        b = 0 + tuple_b[1]
-    tup = (a, b)
-    return(tup)
+    if(len(tuple_a) == 0):
+        tupleUnoA = 0
+        tupleUnoB = 0
+    if(len(tuple_b) == 0):
+        tupleDosA = 0
+        tupleDosB = 0
+    if(len(tuple_a) == 1):
+        tupleUnoA = tuple_a[0]
+        tupleUnoB = 0
+    if(len(tuple_b) == 1):
+        tupleDosA = tuple_b[0]
+        tupleDosB = 0
+    if(len(tuple_a) >= 2):
+        tupleUnoA = tuple_a[0]
+        tupleUnoB = tuple_a[1]
+    if(len(tuple_b) >= 2):
+        tupleDosA = tuple_b[0]
+        tupleDosB = tuple_b[1]
+    a = tupleUnoA + tupleDosA
+    b = tupleUnoB + tupleDosB
+    tupp = (a, b)
+    return (tupp)

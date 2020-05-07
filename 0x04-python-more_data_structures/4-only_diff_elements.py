8bcefd518f342230480f0569b5fe021f31ff3951
Nicolanz <zarateguarinnicolasandres@gmail.com>
2020-05-07 23:00:28 +0000

    Task #4

---
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+def only_diff_elements(set_1, set_2):
+    new = set()
+    for i in set_1:
+        for j in set_2:
+            if (i == j):
+                break
+            else:
+                continue
+        if(i != j):
+            new.add(i)
+    j = 0
+    i = 0
+    for i in set_2:
+        for j in set_1:
+            if (i == j):
+                break
+            else:
+                continue
+        if(i != j):
+            new.add(i)
+    return(new)

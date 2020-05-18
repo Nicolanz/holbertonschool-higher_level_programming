74744b5c7e7e384aafb2e140a06f584b94f9f09f
Nicolanz <zarateguarinnicolasandres@gmail.com>
2020-05-18 22:11:35 +0000

    Task #2

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    b = 0
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            b = b + 1
+        except (ValueError, TypeError):
+            pass
+    print()
+    return(b)

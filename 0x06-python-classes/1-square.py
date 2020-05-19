5c8357e40e90137e2f3d4da2778c3ddbb48a4311
Nicolanz <zarateguarinnicolasandres@gmail.com>
2020-05-19 22:18:26 +0000

    Task #1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
+"""1-square - Crates a class with private attribute size"""
+
+
 class Square:
+    """Class Square"""
     def __init__(self, size):
+        """method is run as soon as an object of a class is instantiated
+
+        Arguments:
+            size {[int]} -- Private variable
+        """
         self.__size = size

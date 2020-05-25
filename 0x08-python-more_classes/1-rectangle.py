0a1c0afb04b67a89a7ded5569b9608053709a32a
Nicolanz <zarateguarinnicolasandres@gmail.com>
2020-05-25 21:05:27 +0000

    Task #0 and 1

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,15 +1,41 @@
 #!/usr/bin/python3
+"""Module to define a rectangle with
+height and width specified"""
+
+
 class Rectangle:
+    """Class the built the rectangle"""
     def __init__(self, width=0, height=0):
+        """Function to Initialize the atributtes
+
+        Keyword Arguments:
+            width {int} -- [width of the Ractangle] (default: {0})
+            height {int} -- [Height of the Rectangle] (default: {0})
+        """
         self.__width = width
         self.__height = height
 
     @property
     def width(self):
+        """Returns the width of the Rectangle
+
+        Returns:
+            [int] -- [Return width to the modify it]
+        """
+
         return self.__width
 
     @width.setter
     def width(self, value):
+        """Function to modify width
+
+        Arguments:
+            value {[int]} -- [New value]
+
+        Raises:
+            TypeError: [Catchs type errors]
+            ValueError: [Catchs value errors]
+        """
         if (type(value) != int):
             raise TypeError("width must be an integer")
         elif (value < 0):
@@ -19,10 +45,24 @@ class Rectangle:
 
     @property
     def height(self):
+        """Returns the height of the rectangle
+
+        Returns:
+            [int] -- [New value]
+        """
         return self.__width
 
     @height.setter
     def height(self, value):
+        """Funtion to modify the value
+
+        Arguments:
+            value {[int]} -- [New value]
+
+        Raises:
+            TypeError: [Catchs value errors]
+            TypeError: [Catchs type errors]
+        """
         if (type(value) != int):
             raise TypeError("height must be an integer")
         elif (value < 0):

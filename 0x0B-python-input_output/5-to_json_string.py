33bb767204b27e4fd83a98502cca3fa5501b29b1
Nicolanz <zarateguarinnicolasandres@gmail.com>
2020-06-03 22:59:29 +0000

    Task #5

---
+++ 0x0B-python-input_output/5-to_json_string.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import json
+"""Return json repr of an object"""
+
+
+def to_json_string(my_obj):
+    """Converts to json string
+
+    Arguments:
+        my_obj {[class]} -- [object]
+
+    Returns:
+        [type] -- [class]
+    """
+    return json.dumps(my_obj)

e75dffb9c6f85d3660a1587e819e905f10f84550
Nicolanz <zarateguarinnicolasandres@gmail.com>
2020-06-03 03:04:50 +0000

    Task #8

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Module to create a new rectangle"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Rectangle Class
+
+    Arguments:
+        BaseGeometry {[class]} -- [SuperClass]
+    """
+    def __init__(self, width, height):
+        """Constructor
+
+        Arguments:
+            width {[int]} -- [positive ints]
+            height {[int]} -- [positive ints]
+        """
+        self.__width = width
+        self.__height = height
+        Rectangle.integer_validator(self, width, height)

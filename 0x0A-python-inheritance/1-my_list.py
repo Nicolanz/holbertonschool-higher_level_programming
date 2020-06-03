f16aa84b2ae4d43147b141b9fae921ca39462548
Nicolanz <zarateguarinnicolasandres@gmail.com>
2020-06-03 02:22:09 +0000

    Task #1 updated

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,6 +1,15 @@
 #!/usr/bin/python3
+"""Module which creates an inherited class of list"""
+
+
 class MyList(list):
+    """Class to Initialize the class
+
+    Arguments:
+        list {[list]} -- [list]
+    """
     def print_sorted(self):
+        """Prints lists sorted"""
         lista = self.copy()
         lista.sort()
         print(lista)

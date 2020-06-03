aeac5cc3d13042fa8ced8daf45dbaa13944f1b15
Nicolanz <zarateguarinnicolasandres@gmail.com>
2020-06-03 22:20:21 +0000

    Task #3

---
+++ 0x0B-python-input_output/3-write_file.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Writes a string to a text file"""
+
+
+def write_file(filename="", text=""):
+    """Writes a string to a text file
+    and creates it if it does not exist
+
+    Keyword Arguments:
+        filename {str} -- [Name of the file] (default: {""})
+        text {str} -- [Text] (default: {""})
+
+    Returns:
+        [int] -- [Number of characters written]
+    """
+    lines = 0
+    with open(filename, "w") as f:
+        for i in text:
+            f.write(i)
+            lines += 1
+    return lines

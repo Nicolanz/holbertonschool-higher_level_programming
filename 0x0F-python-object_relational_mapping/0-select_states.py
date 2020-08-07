f228e35ae2a7f0d2804ccbb562adc52a76b6dbdc
Nicolanz <zarateguarinnicolasandres@gmail.com>
2020-08-07 23:24:07 +0000

    Tasks 0 & 1

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,1 +1,23 @@
 #!/usr/bin/python3
+"""Module to list a database"""
+from sys import argv, exit
+import MySQLdb
+
+if (len(argv) < 4):
+    pass
+else:
+    try:
+        db = MySQLdb.connect(
+            host="localhost",
+            user=argv[1],
+            passwd=argv[2],
+            db=argv[3]
+        )
+    except:
+        exit(1)
+    cursor = db.cursor()
+    cursor.execute('SELECT * FROM states')
+    result = cursor.fetchall()
+    for i in result:
+        print(i)
+    db.close()
--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,1 +1,25 @@
 #!/usr/bin/python3
+"""Module to list states which have a N as initia
+vpepalue"""
+from sys import argv, exit
+import MySQLdb
+
+if (len(argv) < 4):
+    pass
+else:
+    try:
+        db = MySQLdb.connect(
+            host="localhost",
+            user=argv[1],
+            passwd=argv[2],
+            db=argv[3]
+        )
+    except:
+        exit(1)
+    cursor = db.cursor()
+    cursor.execute('SELECT * FROM states')
+    result = cursor.fetchall()
+    for i in result:
+        if i[1][0] == "N":
+            print(i)
+    db.close()

9c13a1f4536964b59efc94bdb19597ca967ab554
Nicolanz <zarateguarinnicolasandres@gmail.com>
2020-08-08 22:40:06 +0000

    Task 4

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,1 +1,25 @@
 #!/usr/bin/python3
+"""Moudule to list cities from a table"""
+from sys import argv, exit
+import MySQLdb
+
+if len(argv) < 4:
+    pass
+else:
+    try:
+        db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=argv[1],
+            passwd=argv[2],
+            db=argv[3]
+        )
+    except:
+        exit(1)
+    cursor = db.cursor()
+    cursor.execute('SELECT cities.id, cities.name, states.name\
+        FROM cities INNER JOIN states ON cities.state_id = states.id')
+    result = cursor.fetchall()
+    for i in result:
+        print(i)
+    db.close()

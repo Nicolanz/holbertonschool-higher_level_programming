abbe81a53f346c365e4022be476ac7092a577d56
Nicolanz <zarateguarinnicolasandres@gmail.com>
2020-08-08 23:42:21 +0000

    Task 3

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,1 +1,25 @@
 #!/usr/bin/python3
+"""Module to list matches with the state given
+as arg"""
+from sys import argv, exit
+import MySQLdb
+
+if (len(argv) < 5):
+    pass
+else:
+    try:
+        db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=argv[1],
+            passwd=argv[2],
+            db=argv[3]
+        )
+    except:
+        exit(1)
+    with db.cursor() as cursor:
+        cursor.execute("SELECT * FROM states WHERE name =\
+            '{:s}'".format(argv[4]))
+        result = cursor.fetchall()
+    for i in result:
+        print(i)

de17cefe36ca36bfe3468b9a26e721034d781449
Nicolanz <zarateguarinnicolasandres@gmail.com>
2020-08-09 21:55:40 +0000

    Task 7

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -7,20 +7,16 @@ from model_state import Base, State
 
 if __name__ == "__main__":
     engine = sqlalchemy.create_engine(
-        'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        'mysql+mysqldb://{}:{}@localhost:/{}'.format(
             sys.argv[1],
             sys.argv[2],
-            sys.argv[3]),
-        pool_pre_ping=True
+            sys.argv[3]), pool_pre_ping=True
     )
 
     Session = sessionmaker(engine)
     session = Session()
     consulta = session.query(State).all()
 
-    if len(consulta) > 0:
-        for i in range(len(consulta)):
-            print ("{:d}: ".format(consulta[i].__dict__['id']), end='')
-            print ("{:s}".format(consulta[i].__dict__['name']))
-    else:
-        print()
+    for i in range(len(consulta)):
+        print ("{}: ".format(consulta[i].__dict__['id']), end='')
+        print ("{}".format(consulta[i].__dict__['name']))

ba2714126c962d5d86a927649f3b805e231fdf73
Nicolanz <zarateguarinnicolasandres@gmail.com>
2020-08-09 22:47:06 +0000

    Task 8

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,1 +1,25 @@
 #!/usr/bin/python3
+"""Module to list the first State object"""
+import sys
+from sqlalchemy import create_engine
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(
+            sys.argv[1],
+            sys.argv[2],
+            sys.argv[3]), pool_pre_ping=True
+    )
+    Session = sessionmaker(engine)
+    session = Session()
+    consulta = session.query(State).get(1)
+
+    if len(session.query(State).all()) > 0:
+        print("{:d}: {:s}".format(
+            consulta.__dict__['id'], consulta.__dict__['name']
+            )
+        )
+    else:
+        print()

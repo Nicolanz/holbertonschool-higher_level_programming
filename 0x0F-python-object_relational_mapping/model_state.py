07b1f1b59507f6517ce671873b65a820c99aeb38
Nicolanz <zarateguarinnicolasandres@gmail.com>
2020-08-09 20:12:39 +0000

    Task model

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -27,13 +27,3 @@ class State(Base):
         sqlalchemy.String(128),
         nullable=False
     )
-
-if __name__ == "__main__":
-    engine = create_engine(
-        'mysql+mysqldb://{}:{}@localhost/{}'.format(
-            sys.argv[1],
-            sys.argv[2],
-            sys.argv[3]
-            ), pool_pre_ping=True
-        )
-    Base.metadata.create_all(engine)

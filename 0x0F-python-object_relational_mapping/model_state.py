b9917fdf1c75d90ede8484e4f65e387dc2cb3f26
Nicolanz <zarateguarinnicolasandres@gmail.com>
2020-08-09 18:16:26 +0000

    Model state again

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -2,11 +2,17 @@
 """Module of model state"""
 import sqlalchemy
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import (create_engine)
 
 Base = declarative_base()
 
 
 class State(Base):
+    """Class State
+
+    Args:
+        Base ([base]): [Base class]
+    """
     __tablename__ = 'states'
     id = sqlalchemy.Column(
         'id',
@@ -20,3 +26,7 @@ class State(Base):
         sqlalchemy.String(128),
         nullable=False
     )
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)

a378f4a9740b7782caae1cb7c943aacc3e95c527
Nicolanz <zarateguarinnicolasandres@gmail.com>
2020-08-09 21:28:39 +0000

    Task 7

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -13,8 +13,8 @@ if __name__ == "__main__":
             sys.argv[3]),
         pool_pre_ping=True
     )
+    Base.metadata.create_all(engine)
 
-Base.metadata.create_all(engine)
 Session = sessionmaker(bind=engine)
 session = Session()
 consulta = session.query(State).all()

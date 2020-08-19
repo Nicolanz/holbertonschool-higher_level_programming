879f42a05f9d459d444f8d6c233a96c4eb5615bd
Nicolanz <zarateguarinnicolasandres@gmail.com>
2020-08-19 00:28:01 +0000

    Task 5

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Post request
-curl -d "email=hr@holbertonschool.com&&subject=I will always be here for PLD" -sX POST $1;
+curl -sd 'email=hr@holbertonschool.com&subject=I will always be here for PLD' $1

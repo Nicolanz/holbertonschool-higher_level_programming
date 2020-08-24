d7eed75002525200c8bf21ab8f4f556873f38fb3
Nicolanz <zarateguarinnicolasandres@gmail.com>
2020-08-24 23:09:22 +0000

    Task 4

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+"""request using requests"""
+import requests
+
+if __name__ == "__main__":
+    obj = requests.get('https://intranet.hbtn.io/status')
+    print(
+        "Body response:\n\t- type: {}\n\t- content: {}"
+        .format(type(obj.text), obj.text)
+        )

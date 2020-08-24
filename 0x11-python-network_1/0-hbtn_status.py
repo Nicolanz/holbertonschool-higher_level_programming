ce52bd55d21f674019047b6d6e2f89375090d8df
Nicolanz <zarateguarinnicolasandres@gmail.com>
2020-08-24 19:59:16 +0000

    0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
 """Print body of a request"""
-import urllib.request
+from urllib import request
 
 if __name__ == "__main__":
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as obj:
+    with request.urlopen('https://intranet.hbtn.io/status') as obj:
         req = obj.read()
 
-    print("Body response:\n\
-    \t- type: {}\n\
-    \t- content: {}\n\
-    \t- utf8 content: {}".format(type(req), req, req.decode("UTF-8")))
+    print(
+        "Body response:\n\t- type: {}\n\t- content: {}\n\t- utf8 content: {}"
+        .format(
+            type(req),
+            req,
+            req.decode("UTF-8")
+        )
+    )

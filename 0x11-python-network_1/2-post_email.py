91c8b95079972817f7ecf3465e3fde58fead9bf4
Nicolanz <zarateguarinnicolasandres@gmail.com>
2020-08-24 22:09:24 +0000

    Task 2

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+"""Post an email to a website"""
+from sys import argv
+import urllib.parse
+import urllib.request
+
+site = argv[1]
+email = {'email': argv[2]}
+
+
+data = urllib.parse.urlencode(email)
+mail = data.encode('utf-8')
+
+req = urllib.request.Request(site, mail)
+
+with urllib.request.urlopen(req) as obj:
+    body = obj.read()
+    print(body.decode('utf-8'))

dacba33ef3fbb56cf8cd534717d0ad391273a15e
Nicolanz <zarateguarinnicolasandres@gmail.com>
2020-08-24 23:15:27 +0000

    Task 5

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+"""Header variable with request"""
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    obj = requests.get(argv[1])
+    print(obj.headers['X-Request-Id'])

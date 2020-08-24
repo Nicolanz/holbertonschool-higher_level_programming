0dc39b6b0b0f899cb5639274018f7c69711f1a15
Nicolanz <zarateguarinnicolasandres@gmail.com>
2020-08-24 21:07:04 +0000

    Task 1

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+import urllib.request
+from sys import argv
+"""Displays the X-Request-Id variable found un a header"""
+
+with urllib.request.urlopen(argv[1]) as response:
+    html = response.headers['X-Request-Id']
+    print(html)

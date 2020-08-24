889d977555277595dc2717dd6757e63406ac3614
Nicolanz <zarateguarinnicolasandres@gmail.com>
2020-08-24 23:32:08 +0000

    Task 6

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+"""POST with requests"""
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    url = argv[1]
+    email = {'email': argv[2]}
+    req = requests.post(url, data=email)
+    print(req.text)

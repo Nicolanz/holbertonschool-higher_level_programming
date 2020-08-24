f84eb651e19c629be811fc0403d192a1319c49c8
Nicolanz <zarateguarinnicolasandres@gmail.com>
2020-08-24 19:15:43 +0000

    0-hbtn

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,10 +3,10 @@
 from urllib import request
 
 
-with request.urlopen("https://intranet.hbtn.io/status") as obj:
+with request.urlopen('https://intranet.hbtn.io/status') as obj:
     req = obj.read()
 
-print("Body response:\n\
+    print("Body response:\n\
     \t- type: {}\n\
     \t- content: {}\n\
     \t- utf8 content: {}".format(type(req), req, req.decode("UTF-8")))

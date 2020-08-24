65f00206ae3f70f5690e5529a534f4f46988c177
Nicolanz <zarateguarinnicolasandres@gmail.com>
2020-08-24 22:56:22 +0000

    Task 3

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+"""print status error if exists"""
+from sys import argv
+import urllib.request
+
+if __name__ == "__main__":
+    req = urllib.request.Request(argv[1])
+    try:
+        with urllib.request.urlopen(req) as obj:
+            body = obj.read()
+            print(body.decode('utf-8'))
+    except urllib.error.HTTPError as e:
+        print("Error code: {}".format(e.code))

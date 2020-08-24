dbb4958c9f6a65439f443d382e2e425ba97afb49
Nicolanz <zarateguarinnicolasandres@gmail.com>
2020-08-24 23:46:25 +0000

    7-error

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    obj = requests.get(argv[1])
+    if obj.status_code >= 400:
+        print("Error code {}".format(obj.status_code))
+    else:
+        print(obj.text)

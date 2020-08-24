9c63472bc75d435578a3670579893ec1907fae0f
Nicolanz <zarateguarinnicolasandres@gmail.com>
2020-08-24 22:18:22 +0000

    Task 2

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -5,13 +5,13 @@ import urllib.parse
 import urllib.request
 
 site = argv[1]
-email = {'email': argv[2]}
+mail = {'email': argv[2]}
 
 
-data = urllib.parse.urlencode(email)
-mail = data.encode('utf-8')
+data = urllib.parse.urlencode(mail)
+email = data.encode('utf-8')
 
-req = urllib.request.Request(site, mail)
+req = urllib.request.Request(site, email)
 
 with urllib.request.urlopen(req) as obj:
     body = obj.read()

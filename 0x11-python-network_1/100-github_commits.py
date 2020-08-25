f8c6e8c4d00d3970cec5896caf24f20527c10867
Nicolanz <zarateguarinnicolasandres@gmail.com>
2020-08-25 03:12:10 +0000

    Task 100

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+"""Print last 10 commits with the github API"""
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    repo = argv[1]
+    owner = argv[2]
+    url = "https://api.github.com/repos/{}/{}/commits".format(owner, repo)
+
+    obj = requests.get(url)
+    commitsDict = obj.json()
+
+    # print(commitsDict[1]['sha'])
+
+    for i in range(0, 10):
+        print("{}: {}".format(
+            commitsDict[i]['sha'],
+            commitsDict[i]['commit']['author']['name'])
+        )

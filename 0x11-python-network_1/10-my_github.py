e75372f451fd832e28577e9369a793aceb873329
Nicolanz <zarateguarinnicolasandres@gmail.com>
2020-08-25 01:31:46 +0000

    Task 10

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+"""Get with requests to github"""
+import requests
+from sys import argv, exit
+
+if __name__ == "__main__":
+
+    url = "https://api.github.com/user"
+    user = argv[1]
+    passw = argv[2]
+
+    obj = requests.get(url, auth=(user, passw))
+
+    try:
+        var = obj.json()
+        print(var['id'])
+    except:
+        pass

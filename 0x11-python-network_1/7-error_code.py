0f0e3a2c329aab96a4b4185f7b03a057f2c8c339
Nicolanz <zarateguarinnicolasandres@gmail.com>
2020-08-25 00:01:02 +0000

    Task 7

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,11 +1,21 @@
 #!/usr/bin/python3
-"""7 error with requests"""
+"""7 error with requets"""
 import requests
 from sys import argv
 
 if __name__ == "__main__":
-    obj = requests.get(argv[1])
-    if obj.status_code >= 400:
-        print("Error code {}".format(obj.status_code))
-    else:
-        print(obj.text)
+
+    try:
+        obj = requests.get(argv[1])
+        if obj.status_code >= 400:
+            print("Error code: {:d}".format(obj.status_code))
+        else:
+            print(obj.text)
+    except:
+        pass
+
+    # obj = requests.get(argv[1])
+    # if obj.status_code >= 400:
+    #     print("Error code {}".format(obj.status_code))
+    # else:
+    #     print(obj.text)

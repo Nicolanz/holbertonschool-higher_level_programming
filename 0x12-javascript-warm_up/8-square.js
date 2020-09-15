79c2355f6aa417ed9ec41fbf595a0dd102ed40d9
Nicolanz <zarateguarinnicolasandres@gmail.com>
2020-09-15 21:05:21 +0000

    Task 8

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,1 +1,14 @@
 #!/usr/bin/node
+let i;
+const size = parseInt(process.argv[2], 10);
+
+if (process.argv[2] && isNaN(size) === false) {
+  for (i = 0; i < size; i++) {
+    for (let j = 0; j < size; j++) {
+      process.stdout.write('X');
+    }
+    console.log();
+  }
+} else {
+  console.log('Missing size');
+}

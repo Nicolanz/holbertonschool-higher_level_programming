ce601b0fd5171d37cbb4623b2e1a7069fe52ed18
Nicolanz <zarateguarinnicolasandres@gmail.com>
2020-09-15 20:34:40 +0000

    Task 7

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,1 +1,14 @@
 #!/usr/bin/node
+let i;
+if (process.argv[2]) {
+  const num = parseInt(process.argv[2]);
+  if (isNaN(num)) {
+    console.log('Missing number of occurrences');
+  } else {
+    for (i = 0; i < num; i++) {
+      console.log('C is fun');
+    }
+  }
+} else {
+  console.log('Missing number of occurrences');
+}

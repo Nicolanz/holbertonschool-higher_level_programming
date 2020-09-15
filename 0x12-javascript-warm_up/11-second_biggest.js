f3a02fdc3747cc6e2e0079979bebb2898a054b2e
Nicolanz <zarateguarinnicolasandres@gmail.com>
2020-09-15 21:59:19 +0000

    Task 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-const array = [];
 if ((!process.argv[2]) || (process.argv.length === 3)) {
   console.log(0);
 } else {
+  const array = [];
   for (let i = 2; i < process.argv.length; i++) {
     array.push(parseInt(process.argv[i]));
   }

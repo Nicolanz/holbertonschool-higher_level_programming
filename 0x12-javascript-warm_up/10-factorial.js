5ca7ddbae3813f921fc25992c62f3b0a59eaaf48
Nicolanz <zarateguarinnicolasandres@gmail.com>
2020-09-15 21:25:39 +0000

    Task 10

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+function factorial (num) {
+  if (isNaN(num)) {
+    return (1);
+  } else if (num === 0) {
+    return (1);
+  }
+  return (num * factorial(num - 1));
+}
+const result = factorial(parseInt(process.argv[2]));
+console.log(result);

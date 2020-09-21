372a4c8950c5b596c838ba6d2502885d9b6af592
Nicolanz <zarateguarinnicolasandres@gmail.com>
2020-09-21 22:32:51 +0000

    Task 6

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,1 +1,24 @@
 #!/usr/bin/node
+const Rectangle = require('./4-rectangle');
+
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    let printMe;
+    if (c) {
+      printMe = c;
+    } else {
+      printMe = 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      for (let j = 0; j < this.width; j++) {
+        process.stdout.write(printMe);
+      }
+      console.log();
+    }
+  }
+}
+module.exports = Square;

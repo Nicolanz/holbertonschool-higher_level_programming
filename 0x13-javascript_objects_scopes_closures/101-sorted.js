88c67449dd4d4b667c9a691b30483a615b9a9111
Nicolanz <zarateguarinnicolasandres@gmail.com>
2020-09-22 02:00:50 +0000

    Task 101

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,1 +1,19 @@
 #!/usr/bin/node
+const firstDict = require('./101-data').dict;
+const newList = [];
+const newDict = {};
+let i = 0;
+for (const key in firstDict) {
+  if (!newList.includes(firstDict[key])) {
+    const otherList = [];
+    newList.push(firstDict[key]);
+    for (const otherKey in firstDict) {
+      if (firstDict[otherKey] === newList[i]) {
+        otherList.push(otherKey);
+      }
+    }
+    i++;
+    newDict[firstDict[key]] = otherList;
+  }
+}
+console.log(newDict);

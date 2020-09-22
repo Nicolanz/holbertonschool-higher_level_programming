36bfdd4a446d897ddb1ad34807251894a74f4798
Nicolanz <zarateguarinnicolasandres@gmail.com>
2020-09-22 23:03:08 +0000

    Task 4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,1 +1,18 @@
 #!/usr/bin/node
+if (process.argv[2]) {
+  const request = require('request');
+  const url = process.argv[2].toString();
+  let counter = 0;
+
+  request(url, function (err, res, body) {
+    if (!err) {
+      const list = JSON.parse(body).results;
+      for (let i = 0; i < list.length; i++) {
+        if (list[i].characters.includes('https://swapi-api.hbtn.io/api/people/18/')) {
+          counter++;
+        }
+      }
+      console.log(counter);
+    }
+  });
+}

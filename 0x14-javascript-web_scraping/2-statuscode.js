faff346ce7ae18a82aaec79651750184da1d6670
Nicolanz <zarateguarinnicolasandres@gmail.com>
2020-09-22 20:38:35 +0000

    Task 2

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,1 +1,7 @@
 #!/usr/bin/node
+if (process.argv[2]) {
+  const request = require('request');
+  request(process.argv[2], function (response) {
+    console.log('code: ' + response.statusCode);
+  });
+}

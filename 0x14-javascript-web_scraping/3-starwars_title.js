ce7060f8bfabdd257b2e16f642797ba82efc25ba
Nicolanz <zarateguarinnicolasandres@gmail.com>
2020-09-22 22:30:19 +0000

    Task 3

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,1 +1,12 @@
 #!/usr/bin/node
+if (process.argv[2]) {
+  const request = require('request');
+  const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2].toString();
+
+  request(url, function (err, res, body) {
+    if (err) {
+      console.log(err);
+    }
+    console.log(JSON.parse(body).title);
+  });
+}

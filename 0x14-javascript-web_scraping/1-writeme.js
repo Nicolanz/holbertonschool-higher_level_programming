bb3074b0725a3d23a928c1838b73e0b19370201d
Nicolanz <zarateguarinnicolasandres@gmail.com>
2020-09-22 19:38:42 +0000

    Task 1

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,1 +1,9 @@
 #!/usr/bin/node
+if (process.argv[2]) {
+  const fs = require('fs');
+  fs.writeFile(process.argv[2], process.argv[3], 'utf-8', function (err, file) {
+    if (err) {
+      console.log(err);
+    }
+  });
+}

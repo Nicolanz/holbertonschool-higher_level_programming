2b95328ee8e2e1711a671bbf9433e2cdcc5f7247
Nicolanz <zarateguarinnicolasandres@gmail.com>
2020-09-22 02:35:07 +0000

    Task 102

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,1 +1,13 @@
 #!/usr/bin/node
+if (process.argv[2]) {
+  const fs = require('fs');
+  const buf1 = fs.readFileSync(process.argv[2]);
+  const str1 = buf1.toString();
+  const buf2 = fs.readFileSync(process.argv[3]);
+  const str2 = buf2.toString();
+
+  const myStr = str1.concat(str2);
+  fs.writeFile(process.argv[4], myStr, function (err, file) {
+    if (err) throw err;
+  });
+}

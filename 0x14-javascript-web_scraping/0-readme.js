861e417551c8bac852e0a7121920a5c7bb547942
Nicolanz <zarateguarinnicolasandres@gmail.com>
2020-09-22 19:10:47 +0000

    Task 0

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,1 +1,12 @@
 #!/usr/bin/node
+if (process.argv[2]) {
+  const fs = require('fs');
+
+  fs.readFile(process.argv[2], 'utf-8', (err, data) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(data);
+    }
+  });
+}

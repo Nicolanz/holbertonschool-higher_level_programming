de2de06a5cf6af156c089ccf2d45ebf23a5ed102
Nicolanz <zarateguarinnicolasandres@gmail.com>
2020-09-23 03:05:16 +0000

    Task 6

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -3,24 +3,25 @@ if (process.argv[2]) {
   const request = require('request');
 
   request(process.argv[2], function (err, res, body) {
-    if (err) { console.log(err); }
-    const users = JSON.parse(body);
-    const dict = {};
-    let tasks = 0;
+    if (err) { console.log(err); } else {
+      const users = JSON.parse(body);
+      const dict = {};
+      let tasks = 0;
 
-    for (let i = 0; i < users.length; i++) {
-      if (users[i].completed === true) {
-        tasks++;
+      for (let i = 0; i < users.length; i++) {
+        if (users[i].completed === true) {
+          tasks++;
+        }
+        if (!users[i + 1]) {
+          dict[users[i].userId] = tasks;
+          break;
+        }
+        if (users[i + 1].userId > users[i].userId) {
+          dict[users[i].userId] = tasks;
+          tasks = 0;
+        }
       }
-      if (!users[i + 1]) {
-        dict[users[i].userId] = tasks;
-        break;
-      }
-      if (users[i + 1].userId > users[i].userId) {
-        dict[users[i].userId] = tasks;
-        tasks = 0;
-      }
+      console.log(dict);
     }
-    console.log(dict);
   });
 }

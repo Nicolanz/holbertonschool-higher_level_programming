8c0aa8fc0d96f6b82b2688272930d61a1041684c
Nicolanz <zarateguarinnicolasandres@gmail.com>
2020-09-23 04:20:27 +0000

    Task 100

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,1 +1,17 @@
 #!/usr/bin/node
+if (process.argv[2]) {
+  const request = require('request');
+  const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2].toString();
+
+  request(url, (err, res, body) => {
+    if (err) { console.log(err); }
+    const charList = JSON.parse(body).characters;
+    let i = 0;
+    for (i in charList) {
+      request(charList[i], (error, response, body1) => {
+        if (error) { console.log(error); }
+        console.log(JSON.parse(body1).name);
+      });
+    }
+  });
+}

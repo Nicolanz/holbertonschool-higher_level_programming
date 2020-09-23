f54f7c2bd83a4baa3b25ab92ec5ead445ca9441e
Nicolanz <zarateguarinnicolasandres@gmail.com>
2020-09-23 00:40:14 +0000

    Task 5

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,1 +1,13 @@
 #!/usr/bin/node
+if (process.argv[2]){
+    const request = require('request');
+    const fs = require('fs');
+
+    request(process.argv[2], function (err, res, body) {
+        fs.writeFile(process.argv[3], body, 'utf-8', function (err, file) {
+            if (err) {
+                console.log(err);
+            }
+        });
+    });
+}
